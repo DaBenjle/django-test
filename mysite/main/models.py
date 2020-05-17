e4e859b1a3eaa22b75e18f673e27979a41cf86cd
Ben Hilton <tbehilton@gmail.com>
2020-05-17 15:55:22 -0600

    ReCompartimentalized and restarted auth compatibility.

---
+++ mysite/main/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class ToDoList(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class ToDoItem(models.Model):
+    text = models.CharField(max_length=400)
+    toDoList = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return text
